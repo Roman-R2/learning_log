f32483505f85c33af74448116d7dc726ce943190
Roman <slyusarrv@yandex.ru>
2021-11-08 12:57:41 +0500

    Added custom 404 and 500 error pages.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -132,6 +132,7 @@ LOGIN_URL = '/users/login/'
 
 # Настройки Heroku
 import django_heroku
+
 django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -22,7 +22,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+    # topic = Topic.objects.get(id=topic_id)
     # Проверка того, что тема принадлежит текущему пользователю.
     check_topic_owner(topic.owner, request.user)
 
